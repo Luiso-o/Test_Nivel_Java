62827fdeda2c13159c37bb63ecf0101275fa3aed
Luiso-o <cheportillo@gmail.com>
2023-12-28 13:01:32 +0100

    ejercicio18

---
+++ src/Ejercicio_18.java
@@ -0,0 +1,18 @@
+/*Completa la respuesta para que el ejercicio sea válido
+A a = new A()
+A b = new B()
+*/
+public class Ejercicio_18 {
+    public static void main(String[] args) {
+        A a = new A();
+        A b = new B();
+    }
+}
+
+class A{
+    public A(){}
+}
+
+class B extends A{
+    public B(){}
+}
